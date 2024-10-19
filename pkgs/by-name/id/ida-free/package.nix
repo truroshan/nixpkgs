1aae73ba66ab3aac49bf8c2d0382149f58098f32
Roshan Rajak l byteio.ɪn <85359493+truroshan@users.noreply.github.com>
2024-10-19 08:20:34 +0530

    Update package.nix

--- pkgs/by-name/id/ida-free/package.nix
+++ pkgs/by-name/id/ida-free/package.nix
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
     urls = [
       "https://web.archive.org/web/20221105181231if_/https://hex-rays.com/products/ida/news/8_1/images/icon_free.png"
     ];
-    hash = "sha256-widkv2VGh+eOauUK/6Sz/e2auCNFAsc8n9z0fdrSnW0=";
+    hash = "sha256-uoZyMUhKY/anBf4p4XwQ1I5VmYqviO4TQa6H2yge8wQ=";
   };
 
   desktopItem = makeDesktopItem {
